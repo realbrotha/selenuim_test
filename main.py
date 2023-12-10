fdc5247c719b0375ee62d1e9a2fb273a28fede4f
HyungWoo Lee <realbrosss@naver.com>
2023-12-10 15:07:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import argparse
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -6,6 +7,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import pyperclip
+
+parser = argparse.ArgumentParser()
+parser.add_argument("keyword", help = "input keyword")
+args = parser.parse_args()
+#keyword = "목동맛집"
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
@@ -17,19 +23,23 @@ chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
 #driver.get ("https://www.naver.com")
 
 browser = webdriver.Chrome(options=chrome_options)
-keyword = "오목교맛집"
-search_blog_link = f"https://search.naver.com/search.naver?where=blog&sm=tab_viw.blog&query={keyword}"
+search_blog_link = f"https://search.naver.com/search.naver?where=blog&sm=tab_viw.blog&query={args.keyword}"
 browser.get(search_blog_link)
 
 
 first_posting = "#sp_blog_1 > div > div.detail_box > div.title_area > a"
 blogger_id_list = []
 WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, first_posting)))
-for idx in range (1,30) :
+for _ in range(3) :
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(2)
+
+for idx in range (1,100) :
     blogger_selector = f"#sp_blog_{idx} > div > div.detail_box > div.title_area > a"
     blog_a_link = browser.find_element(By.CSS_SELECTOR, blogger_selector)
     blog_real_link = blog_a_link.get_attribute("href")
-    blogger_id_list.append(blog_real_link.split("/")[3])
-print("오목교맛집 email ", blogger_id_list)
+    blogger_id_list.append(blog_real_link.split("/")[3] + "@naver.com")
+print(args.keyword, " email ", blogger_id_list)
+browser.quit()
 
 time.sleep(3)
